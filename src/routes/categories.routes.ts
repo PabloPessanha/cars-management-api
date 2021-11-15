cfeba2f3b64ffa137668e316e069442b94ebf87d
Pablo Pessanha <pablopessanh@gmail.com>
2021-11-15 19:57:04 -0300

    feat(categories): added get method categories to list all them

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const categoriesRoute = Router();
 
 const categories = [];
 
+categoriesRoute.get('/categories', (req: Request, res: Response) => {
+  return res.status(200).json(categories);
+});
+
 categoriesRoute.post('/categories', (req: Request, res: Response) => {
   const { name, description } = req.body;
-  const newCategory = { name, description };
+  const newCategory = { id: uuid(), name, description };
 
   categories.push(newCategory);
   return res.status(201).json(newCategory);
