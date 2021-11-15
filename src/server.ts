9964e6f2852e4ffa776e80040fa80e38aba5f455
Pablo Pessanha <pablopessanh@gmail.com>
2021-11-15 19:54:36 -0300

    feat(packages): add json configuration; add uuid package

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,6 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
-
-app.get('/', (_req, res) => {
-  return res.status(200).json({ message: 'ok' });
-});
+app.use(express.json());
 
 app.listen(3333);
