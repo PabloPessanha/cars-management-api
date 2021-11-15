60346650b776171471ad0cfa4ae76e9e0c2341e6
Pablo Pessanha <pablopessanh@gmail.com>
2021-11-15 20:21:43 -0300

    chore(lint): disable prefer default export rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'class-methods-use-this': ['off'],
     'arrow-body-style': ['error', 'always'],
     'no-undef': 'off',
+    'import/prefer-default-export': 'off',
     camelcase: ['off'],
   },
   settings: {
