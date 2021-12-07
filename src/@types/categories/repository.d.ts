bd41beaf90796a4a988222513c681b9c23913cd8
Pablo Pessanha <pablopessanh@gmail.com>
2021-12-06 23:04:53 -0300

    fix: resolve class import issues

--- src/@types/categories/repository.d.ts
+++ src/@types/categories/repository.d.ts
@@ -1,6 +1,5 @@
-import { Category } from '../../modules/cars/model/Category';
-
 declare namespace ICategories {
+  type Category = import('../../modules/cars/model/Category');
   interface Repository {
     findByName(name: string): boolean;
     list(): Category[];
