ece997452f72dd332383804025d3a4a14f0c44be
Pablo Pessanha <pablopessanh@gmail.com>
2021-12-12 15:36:58 -0300

    refactor: change routes to useCase methods

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,13 @@
 import { Router, Request, Response } from 'express';
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createaCategory/CreateCategoryUseCase';
+import { categoriesRepository, createCategoriesController } from '../modules/cars/useCases/createaCategory';
 
 const categoriesRouter = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.get('/', (req: Request, res: Response) => {
   const allCategories = categoriesRepository.list();
   return res.status(200).json(allCategories);
 });
 
-categoriesRouter.post('/', (req: Request, res: Response) => {
-  const { name, description } = req.body;
-  const categoriesService = new CreateCategoryUseCase(categoriesRepository);
-
-  const newCategory = categoriesService.execute({ name, description });
-  return res.status(201).json(newCategory);
-});
+categoriesRouter.post('/', createCategoriesController.handle);
 
 export { categoriesRouter };
