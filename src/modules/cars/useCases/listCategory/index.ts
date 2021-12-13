c74532177e3f7725fbf76a0d7ba9bd30a5861c38
Pablo Pessanha <pablopessanh@gmail.com>
2021-12-12 23:40:57 -0300

    refactor: change new instance to a singleton getter

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { ListCategoryUseCase } from './ListCategoryUseCase';
 import { ListCategoryController } from './ListCategoryController';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstace();
 const listCategoryUseCase = new ListCategoryUseCase(categoriesRepository);
 const listCategoryController = new ListCategoryController(listCategoryUseCase);
 
