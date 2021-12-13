a6e203d5cbaecc955985a86afb6ee1683ee44594
Pablo Pessanha <pablopessanh@gmail.com>
2021-12-13 00:10:44 -0300

    refactor: change router path to useCases handlers

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,10 @@
-import { Router, Request, Response } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { Router } from 'express';
+import { listSpecificationController } from '../modules/cars/useCases/specifications/listSpecification';
+import { createSpecificationController } from '../modules/cars/useCases/specifications/createSpecification';
 
 const specificationsRouter = Router();
-const specificationsRepository = new SpecificationsRepository();
 
-specificationsRouter.get('/', (req: Request, res: Response) => {
-  const allCategories = specificationsRepository.list();
-  return res.status(200).json(allCategories);
-});
-
-specificationsRouter.post('/', (req: Request, res: Response) => {
-  const { name, description } = req.body;
-  const specificationService = new CreateSpecificationService(specificationsRepository);
-
-  const newCategory = specificationService.execute({ name, description });
-  return res.status(201).json(newCategory);
-});
+specificationsRouter.get('/', listSpecificationController.handle);
+specificationsRouter.post('/', createSpecificationController.handle);
 
 export { specificationsRouter };
